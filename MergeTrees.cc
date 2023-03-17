d8b2c70da06dcfa5b80b6c4c72307a4195891b34
Luca Barioglio <luca.barioglio@cern.ch>
2023-03-17 09:30:16 +0100

    Simplify MergeTrees.cc

--- MergeTrees.cc
+++ MergeTrees.cc
@@ -3,15 +3,17 @@
 #include <TKey.h>
 
 #include <iostream>
+#include <string>
 
 // loop over AO2D directories and merge the trees into a single one
 
-void MergeTrees(const std::string &inputFileName, const std::string &treeName, const std::string &outputFileName)
+void MergeTrees(const std::string &inputFileName, const std::string &outputFileName, bool isMC = false)
 {
     TFile inputFile(inputFileName.c_str());
     TDirectory *dir = gDirectory;
     TList *keys = dir->GetListOfKeys();
     TList *outlist = new TList;
+    std::string treeName = (isMC) ? "O2mchypcands" : "O2datahypcands";
     for (int i = 0; i < keys->GetEntries(); i++)
     {
         TKey *key = (TKey *)keys->At(i);
