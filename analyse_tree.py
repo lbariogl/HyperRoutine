3c73b4c98b5a2de37fe0088e7e53fa9f2d34de42
fmazzasc <fmazzasc@cern.ch>
2023-03-23 12:48:19 +0100

    Add merger + config pass3 + dec radius

--- analyse_tree.py
+++ analyse_tree.py
@@ -3,11 +3,12 @@ import uproot
 import pandas as pd
 import argparse
 import yaml
+from hipe4ml.tree_handler import TreeHandler
 
 parser = argparse.ArgumentParser(description='Configure the parameters of the script.')
 parser.add_argument('--mc', dest='mc', action='store_true', help="if True MC information is stored.")
 parser.set_defaults(mc=False)
-parser.add_argument('--input-file', dest='input_file', help="path to the input file.")
+parser.add_argument('--input-files', dest='input_files', help="path to the input files.")
 parser.set_defaults(input_file='../data/AO2D_merged.root')
 parser.add_argument('--output-dir', dest='output_dir', help="path to the directory in which the output is stored.")
 parser.set_defaults(output_dir='../results/')
@@ -21,7 +22,7 @@ args = parser.parse_args()
 
 # initialise parameters from parser (can be overwritten by external yaml file)
 mc = args.mc
-input_file_name = args.input_file
+input_file_name = args.input_files
 output_dir_name = args.output_dir
 output_file_name = args.output_file
 selections = args.selection
@@ -30,7 +31,7 @@ if  args.config_file != "":
     config_file = open(args.config_file, 'r')
     config = yaml.full_load(config_file)
     mc = config['mc']
-    input_file_name = config['input_file']
+    input_files_name = config['input_files']
     output_dir_name = config['output_dir']
     output_file_name = config['output_file']
     selections = config['selection']
@@ -54,6 +55,8 @@ hNTPCclus = ROOT.TH1F("hNTPCclus", ";n TPC clusters", 50, 60, 200)
 hMass3LH = ROOT.TH1F("h_3lh_mass", "; m({}^{3}_{#Lambda}H) (GeV/#it{c})", 50, 2.96, 3.04)
 hMass4LH = ROOT.TH1F("h_4lh_mass", "; m({}^{4}_{#Lambda}H) (GeV/#it{c})", 50, 3.96, 4.04)
 hPtRec = ROOT.TH1F("hPtRec", ";#it{p}_{T} (GeV/#it{c})", 50, 0, 10)
+hRadius = ROOT.TH1F("hRadius", ";Radius (cm)", 100, 0, 40)
+hNSigHe = ROOT.TH1F("hNSigmaHe", ";n #sigma He3", 50, -3, 3)
 
 ## for MC only
 hPtGen = ROOT.TH1F("hPtGen", "; Pt gen", 50, 0, 10)
@@ -65,15 +68,12 @@ hResolutionDecVtxY = ROOT.TH1F("hResolutionDecVtxY", "; Resolution Dec Y", 50, -
 hResolutionDecVtxZ = ROOT.TH1F("hResolutionDecVtxZ", "; Resolution Dec Z", 50, -0.2, 0.2)
 
 # creating the dataframe
-pd_arr = []
-data_events = uproot.open(f'{input_file_name}:{tree_name_data}')
-pd_arr.append(data_events.arrays(library='pd'))
-mc_events = None
-if mc:
-    pd_arr.append(mc_events.arrays(library='pd'))
+tree_hdl = TreeHandler(input_files_name, tree_name_data)
+df = tree_hdl.get_data_frame()
 
-df = pd.concat(pd_arr, ignore_index=True)
+
 df.eval('pt = sqrt(fPx**2 + fPy**2)', inplace=True)
+df.eval('fDecRad = sqrt(fXDecVtx**2 + fYDecVtx**2)', inplace=True)
 if mc:
     df.eval('pt_gen = sqrt(fGenPx**2 + fGenPy**2)', inplace=True)
     df.query('fIsReco==True', inplace=True)
@@ -87,12 +87,16 @@ if mc:
 # filtering
 df_filtered = df.query(selections)
 
+
 ## fill histograms
 fill_th1_hist(hPtRec, df_filtered, 'pt')
 fill_th1_hist(hCosPA, df_filtered, 'fCosPA')
+fill_th1_hist(hRadius, df_filtered, 'fDecRad')
 fill_th1_hist(hNTPCclus, df_filtered, 'fNTPCclusHe')
+fill_th1_hist(hNSigHe, df_filtered, 'fNSigmaHe')
 fill_th1_hist(hMass3LH, df_filtered, 'fMassH3L')
 fill_th1_hist(hMass4LH, df_filtered, 'fMassH4L')
+
 
 if mc:
     fill_th1_hist(hPtGen, df_filtered, 'pt_gen')
@@ -107,7 +111,9 @@ if mc:
 f = ROOT.TFile(f"{output_dir_name}/{output_file_name}", "RECREATE")
 
 hCosPA.Write()
+hRadius.Write()
 hNTPCclus.Write()
+hNSigHe.Write()
 hPtRec.Write()
 hMass3LH.Write()
 hMass4LH.Write()
