0d3460018129a00cf4b2c03d33fa4cd1bb9bca4c
Luca Barioglio <luca.barioglio@cern.ch>
2023-05-08 14:42:29 +0200

    Adapt to new path

--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,6 @@
 # o2-analysis-collision-converter -b --configuration json://configuration.json --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 |
 # o2-analysis-track-propagation -b --configuration json://configuration.json --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 |
 # o2-analysis-trackselection -b --configuration json://configuration.json --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 |
-o2-analysis-event-selection -b --configuration json://configuration.json --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 |
-o2-analysis-lf-hypertriton-reco-task -b --configuration json://configuration.json --aod-writer-keep dangling --aod-writer-ntfmerge 300 --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 |
-o2-analysis-timestamp -b --configuration json://configuration.json --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000
+o2-analysis-event-selection -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 |
+o2-analysis-lf-hypertriton-reco-task -b --configuration json://configuration.json --aod-writer-keep dangling --aod-writer-ntfmerge 300 --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 |
+o2-analysis-timestamp -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000
