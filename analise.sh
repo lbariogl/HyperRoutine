d689a61496ba0a15c118e3281509fbbfc06d7229
Luca Barioglio <luca.barioglio@cern.ch>
2023-06-16 11:07:18 +0200

    Add QA plots to analise.sh

--- analise.sh
+++ analise.sh
@@ -1,4 +1,7 @@
 python3 analyse_tree.py --config-file "configs/config_antimatter.yaml"
+python3 QAplots.py --input-file ../results/HypertritonResults_antimatter_pass4.root --output-dir ../results/plots/matter
 python3 analyse_tree.py --config-file "configs/config_matter.yaml"
+python3 QAplots.py --input-file ../results/HypertritonResults_matter_pass4.root
 rm ../results/HypertritonResults_tot_pass4.root
 hadd ../results/HypertritonResults_tot_pass4.root ../results/HypertritonResults_matter_pass4.root ../results/HypertritonResults_antimatter_pass4.root
+python3 QAplots.py --input-file ../results/HypertritonResults_tot_pass4.root --output-dir ../results/plots/tot
