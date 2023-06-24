865f894af2bd67eb2eb075241bf1a996d5b05360
Luca Barioglio <luca.barioglio@cern.ch>
2023-06-24 18:01:02 +0200

    Update systematic_study.py

--- analise.sh
+++ analise.sh
@@ -1,7 +1,7 @@
-python3 analyse_tree.py --config-file "configs/config_antimatter.yaml"
-python3 QAplots.py --input-file ../results/HypertritonResults_antimatter_pass4.root --output-dir ../results/plots/matter
-python3 analyse_tree.py --config-file "configs/config_matter.yaml"
-python3 QAplots.py --input-file ../results/HypertritonResults_matter_pass4.root
-rm ../results/HypertritonResults_tot_pass4.root
-hadd ../results/HypertritonResults_tot_pass4.root ../results/HypertritonResults_matter_pass4.root ../results/HypertritonResults_antimatter_pass4.root
-python3 QAplots.py --input-file ../results/HypertritonResults_tot_pass4.root --output-dir ../results/plots/tot
+python3 analyse_tree.py --config-file "configs/analysis/config_antimatter.yaml" --dump-out-tree
+python3 QAplots.py --input-file ../results/HypertritonResults_antimatter.root --output-dir ../results/plots/antimatter
+python3 analyse_tree.py --config-file "configs/analysis/config_matter.yaml" --dump-out-tree
+python3 QAplots.py --input-file ../results/HypertritonResults_matter.root --output-dir ../results/plots/matter
+rm ../results/HypertritonResults_tot.root
+hadd ../results/HypertritonResults_tot.root ../results/HypertritonResults_matter.root ../results/HypertritonResults_antimatter.root
+python3 QAplots.py --input-file ../results/HypertritonResults_tot.root --output-dir ../results/plots/tot
--- systematic_study.py
+++ systematic_study.py
@@ -45,7 +45,7 @@ df = tree_hdl.get_data_frame()
 ROOT.RooMsgService.instance().setSilentMode(True)
 ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
 
-def systematic_routine(var, arr, sel_string, histo_data, histo_mc, canvas):
+def systematic_routine(var, arr, sel_string, histo_data, histo_mc, canvas, normalise_to_first=True):
     for i, val in tqdm(enumerate(arr), total=len(arr)):
         # data
         presel = sel_string.format(var, val)
@@ -62,8 +62,12 @@ def systematic_routine(var, arr, sel_string, histo_data, histo_mc, canvas):
         histo_mc.SetBinContent(i+1, signal_counts)
         histo_mc.SetBinError(i+1, signal_counts_err)
 
-    histo_mc.Scale(1./histo_mc.GetBinContent(1))
-    histo_data.Scale(1./histo_data.GetBinContent(1))
+    if normalise_to_first:
+        histo_mc.Scale(1./histo_mc.GetBinContent(1))
+        histo_data.Scale(1./histo_data.GetBinContent(1))
+    else:
+        histo_mc.Scale(1./histo_mc.GetBinContent(histo_mc.GetNbinsX()))
+        histo_data.Scale(1./histo_data.GetBinContent(histo_data.GetNbinsX()))
 
     output_file.cd()
     histo_data.Write()
@@ -74,7 +78,7 @@ def systematic_routine(var, arr, sel_string, histo_data, histo_mc, canvas):
     histo_mc.GetYaxis().SetRangeUser(0.,1.2)
     histo_data.Draw('PE SAME')
     histo_data.GetYaxis().SetRangeUser(0.,1.2)
-    legend = ROOT.TLegend(0.2, 0.3, 0.4, 0.4, '', 'brNDC')
+    legend = ROOT.TLegend(0.4, 0.3, 0.6, 0.4, '', 'brNDC')
     legend.SetLineWidth(0)
     legend.AddEntry(histo_data, 'Data', 'L')
     legend.AddEntry(histo_mc, 'MC', 'L')
@@ -85,7 +89,7 @@ def systematic_routine(var, arr, sel_string, histo_data, histo_mc, canvas):
 ##        CosPA         ##
 ##########################
 
-print('\n\nChecking cosPA\n\n')
+print('Checking cosPA')
 
 cosPA_arr = np.linspace(0.985, 1., 75, dtype=np.float64)
 nCosPa_bins = len(cosPA_arr) - 1
@@ -101,3 +105,67 @@ cCosPAcomparison = ROOT.TCanvas('cCosPAcomparison', 'cCosPAcomparison', 800, 600
 sel_string = r'{} > {}'
 
 systematic_routine('fCosPA', cosPA_arr[:-1], sel_string, hDataSigCosPA, hMcSigCosPA, cCosPAcomparison)
+
+##########################
+##      DcaV0Daug       ##
+##########################
+
+print('Checking DcaV0Daug')
+
+DcaV0Daug_arr = np.linspace(0., .3, 30, dtype=np.float64)
+nDcaV0Daug_bins = len(DcaV0Daug_arr) - 1
+
+hDataSigDcaV0Daug = ROOT.TH1F('hDataSigDcaV0Daug', ';DCA_{V0 daughters} (cm); signal fraction', nDcaV0Daug_bins, DcaV0Daug_arr)
+utils.setHistStyle(hDataSigDcaV0Daug, ROOT.kRed+1)
+
+hMcSigDcaV0Daug = ROOT.TH1D('hMcSigDcaV0Daug', ';DCA_{V0 daughters} (cm); signal fraction', nDcaV0Daug_bins, DcaV0Daug_arr)
+utils.setHistStyle(hMcSigDcaV0Daug, ROOT.kAzure+2)
+
+cDcaV0Daugcomparison = ROOT.TCanvas('cDcaV0Daugcomparison', 'cDcaV0Daugcomparison', 800, 600)
+
+sel_string = r'abs({}) < {}'
+
+systematic_routine('fDcaV0Daug', DcaV0Daug_arr[1:], sel_string, hDataSigDcaV0Daug, hMcSigDcaV0Daug, cDcaV0Daugcomparison, normalise_to_first=False)
+
+##########################
+##         DcaHe        ##
+##########################
+
+print('Checking DcaHe')
+
+DcaHe_arr = np.linspace(0., .3, 30, dtype=np.float64)
+nDcaHe_bins = len(DcaHe_arr) - 1
+
+hDataSigDcaHe = ROOT.TH1F('hDataSigDcaHe', ';DCA({}^{3}He) (cm); signal fraction', nDcaHe_bins, DcaHe_arr)
+utils.setHistStyle(hDataSigDcaHe, ROOT.kRed+1)
+
+hMcSigDcaHe = ROOT.TH1D('hMcSigDcaHe', ';DCA({}^{3}He) (cm); signal fraction', nDcaHe_bins, DcaHe_arr)
+utils.setHistStyle(hMcSigDcaHe, ROOT.kAzure+2)
+
+cDcaHecomparison = ROOT.TCanvas('cDcaHecomparison', 'cDcaHecomparison', 800, 600)
+
+sel_string = r'abs({}) < {}'
+
+systematic_routine('fDcaHe', DcaHe_arr[1:], sel_string, hDataSigDcaHe, hMcSigDcaHe, cDcaHecomparison, normalise_to_first=False)
+
+##########################
+##         DcaPi        ##
+##########################
+
+print('CPicking DcaPi')
+
+DcaPi_arr = np.linspace(0., .3, 30, dtype=np.float64)
+nDcaPi_bins = len(DcaPi_arr) - 1
+
+hDataSigDcaPi = ROOT.TH1F('hDataSigDcaPi', ';DCA(#pi) (cm); signal fraction', nDcaPi_bins, DcaPi_arr)
+utils.setHistStyle(hDataSigDcaPi, ROOT.kRed+1)
+
+hMcSigDcaPi = ROOT.TH1D('hMcSigDcaPi', ';DCA(#pi) (cm); signal fraction', nDcaPi_bins, DcaPi_arr)
+utils.setHistStyle(hMcSigDcaPi, ROOT.kAzure+2)
+
+cDcaPicomparison = ROOT.TCanvas('cDcaPicomparison', 'cDcaPicomparison', 800, 600)
+
+sel_string = r'abs({}) < {}'
+
+systematic_routine('fDcaPi', DcaPi_arr[1:], sel_string, hDataSigDcaPi, hMcSigDcaPi, cDcaPicomparison, normalise_to_first=False)
+
